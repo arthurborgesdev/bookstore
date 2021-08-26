00a2bf71c4c6dbd197b1ee9cfff91caf5529a78d
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-26 20:42:14 -0300

    Finish styling the form and applied containers to header, bookstore and form

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,25 @@ import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
   <div className="header-bar">
-    <div className="title-navbar">
-      <h1 className="title-h1">Bookstore CMS</h1>
-      <nav className="nav-links">
-        <ul className="nav-list">
-          <li>
-            <Link to="/">BOOKS</Link>
-          </li>
-          <li>
-            <Link to="/categories">CATEGORIES</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div className="avatar">
-      <span className="avatar-circle">
-        <FontAwesomeIcon icon={faUserAlt} size="lg" color="#0290ff" />
-      </span>
+    <div className="container">
+      <div className="title-navbar">
+        <h1 className="title-h1">Bookstore CMS</h1>
+        <nav className="nav-links">
+          <ul className="nav-list">
+            <li>
+              <Link to="/">BOOKS</Link>
+            </li>
+            <li>
+              <Link to="/categories">CATEGORIES</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="avatar">
+        <span className="avatar-circle">
+          <FontAwesomeIcon icon={faUserAlt} size="lg" color="#0290ff" />
+        </span>
+      </div>
     </div>
   </div>
 );
